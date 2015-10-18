ffab92f007b580852630558d2302617fd4f238c2
Johann Muszynski <johann.muszynski@gmail.com>
2015-10-18 11:54:02 +0300

    actually add Wren script component

---
+++ src/component/WrenScript.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "Wrenly.h"
+
+namespace pg {
+namespace component {
+    
+struct WrenScript {
+    wrenly::Wren vm;
+    wrenly::Method activate;
+    wrenly::Method deactivate;
+    wrenly::Method update;
+};
+    
+}
+}
